b47419c01bb61f8fcba5930b6b6be6f29bbf2889
Yug Ayush <96907682+yugayush@users.noreply.github.com>
2024-02-01 20:45:05 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,36 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6f5049493amsh3e310ed5eddee4cp1a1b9djsn3f58ccc6a060",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather = (city) =>{
+    cityName.innerHTML = city
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city="+city,
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pct.innerHTML = response.cloud_pct;
+    temp.innerHTML = response.temp;
+    feels_like.innerHTML = response.feels_like;
+    humidity.innerHTML = response.humidity;
+    min_temp.innerHTML = response.min_temp;
+    max_temp.innerHTML = response.max_temp;
+    wind_speed.innerHTML = response.wind_speed;
+    sunrise.innerHTML = response.sunrise;
+    sunset.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
+}
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("New York")
